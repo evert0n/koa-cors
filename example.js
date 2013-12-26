cb802826a32efea9a2062a8fc0a6531cf71b8767
Everton Yoshitani <everton@notreve.com>
2013-12-26 19:02:33 +0900

    Adds example.js

---
+++ example.js
@@ -0,0 +1,12 @@
+var koa = require('koa');
+var route = require('koa-route');
+var cors = require('koa-cors');
+var app = koa();
+
+app.use(cors());
+
+app.use(route.get('/', function() {
+  this.body = { msg: 'Hello World!' };
+}));
+
+app.listen(3000);
